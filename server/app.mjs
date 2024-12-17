d88b3aacbd9b6162dd5c13048f0738eef7688548
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2024-12-17 12:57:58 +0200

    Initial commit
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

---
+++ server/app.mjs
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Express!");
+});
+
+app.get("/hello", async function (req, res, next) {
+    const ipLocation = await fetch("http://ip-api.com/json/")
+        .then((res) => res.json())
+        .catch(() => ({status: "fail"}));
+
+    if (ipLocation.status === "fail") {
+        return `Hello! Failed to get the server location :(`;
+    }
+
+    const formattedTime = new Date().toLocaleString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    });
+
+    res.send({
+        message: `Hello ${req.query.name}! This response was served from ${ipLocation.city}, ${ipLocation.country} (${ipLocation.lat}, ${ipLocation.lon}) at ${formattedTime}`,
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080. Check the app on http://localhost:8080");
+});
